2eed91b9ae3f2669af75b7d18a7f14020d3bf0ea
qimamhd <106440499+qimamhd@users.noreply.github.com>
2025-12-31 15:00:30 +0300

    update

--- models/extra_field.py
+++ models/extra_field.py
@@ -6,7 +6,7 @@ from odoo.exceptions import ValidationError
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT as DATETIME_FORMAT
 
 class ExtraFieldSetting(models.Model):
-    _name = 'extra.field.setting'
+    _name = 'sale.extra.field.config'
 
     field_name = fields.Selection([
         ('x_extra_text_1', 'Extra Text 1'),
